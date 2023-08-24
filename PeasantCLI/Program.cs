3cc2dc27c678ee9323f8bd484e1732c9e4c7c01a
BytesCrafter <bytescrafter@gmail.com>
2023-08-25 03:07:57 +0800

    Move the Program.cs to the src directory.

---
+++ PeasantCLI/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program 
+{
+    static void Main(string[] args) 
+    {
+        if (args.Length > 0) 
+        {
+            Program.Process(args);
+        }
+
+        else 
+        {
+            Console.WriteLine("Invalid command!");
+        }
+    }
+
+    protected static void Process(string[] strings) 
+    {
+        if( strings.Length > 0 && strings[0] == "hello" ) {
+            Console.WriteLine("Hello, how can I help you!");
+        }
+        
+        if( strings.Length > 0 && strings[0] == "speedtest" ) {
+            SpeedTest.Start();
+        }
+    }
+}
