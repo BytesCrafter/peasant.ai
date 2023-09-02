df1bb1e47ac104eb28edd6fee06fd19b50c12edb
BytesCrafter <bytescrafter@gmail.com>
2023-09-02 23:39:10 +0800

    Added the model for ip port.

---
+++ Peasant.Core/Models/Port.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peasant.Core.Models
+{
+    public class Port
+    {
+        public string ip { get; set; }
+        public int port { get; set; }
+        public bool isOpen { get; set; }
+
+        public Port(string _ip, int _port, bool _isOpen)
+        {
+            ip = _ip;
+            port = _port;
+            isOpen = _isOpen;
+        }
+    }
+}
